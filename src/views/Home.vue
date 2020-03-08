aa261d6946a0c92c1d2ab64856f4158d73e7038b
Nico <nicosiput@gmail.com>
2020-03-08 22:02:56 +0700

    Upload Project Vue

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderNico />
+    <HeroSlider />
+    <WomenBanner />
+    <InstaNico />
+    <PartnerNico />
+    <FooterNico />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import HeaderNico from "@/components/HeaderNico.vue";
+import HeroSlider from "@/components/HeroSlider.vue";
+import WomenBanner from "@/components/WomenBanner.vue";
+import InstaNico from "@/components/InstaNico.vue";
+import PartnerNico from "@/components/PartnerNico.vue";
+import FooterNico from "@/components/FooterNico.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderNico,
+    HeroSlider,
+    WomenBanner,
+    InstaNico,
+    PartnerNico,
+    FooterNico
+  }
+};
+</script>
